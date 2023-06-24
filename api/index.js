8d0c19b4b0476397aa1b84433e51785ad5e6ee69
amratansh12 <ashri1205@gmail.com>
2023-06-25 01:09:33 +0530

    Changes in vercel.json 4

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
 const app = require('../server')
+const cors = require('cors')
 
+const corsOptions = {
+    "origin": "*",
+    "methods": "GET, HEAD, PUT, PATCH, POST, DELETE",
+    // other options
+}
+
+index.use(cors(corsOptions));
 module.exports = app;
